d92532e10184187a656e80d0f93dc9718616d2c9
ckstmznf <ckstmznf@naver.com>
2021-09-21 16:21:29 +0900

    Todo list completion
    운동 리스트 추가, 진짜 완성

--- app/src/main/java/com/example/a2021edcanvactionproject/TodoListAdapter.kt
+++ app/src/main/java/com/example/a2021edcanvactionproject/TodoListAdapter.kt
@@ -7,10 +7,11 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a2021edcanvactionproject.model.Todo
 
-class TodoListAdapter(val items: List<Todo>) : RecyclerView.Adapter<TodoListAdapter.TodoListViewHolder>() {
+class TodoListAdapter(val items: MutableList<Todo>) : RecyclerView.Adapter<TodoListAdapter.TodoListViewHolder>() {
     class TodoListViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
         val title = view.findViewById<TextView>(R.id.txt_todoListItem_kind)
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
         val layout = LayoutInflater.from(parent.context).inflate(R.layout.todo_list_item, parent, false)
@@ -24,5 +25,10 @@ class TodoListAdapter(val items: List<Todo>) : RecyclerView.Adapter<TodoListAdap
     override fun getItemCount(): Int {
         return items.size
     }
+
+//    fun add(todo : Todo){
+//        items.add(todo)
+//        notifyItemInserted()
+//    }
 
 }
