36ab675687519dac865137464e2a6a3dc2f4ab63
ckstmznf <ckstmznf@naver.com>
2021-10-28 19:10:34 +0900

    Add todo goto mainFragment

--- app/src/main/java/com/example/a2021edcanvactionproject/Activity/Main/MainActivity.kt
+++ app/src/main/java/com/example/a2021edcanvactionproject/Activity/Main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a2021edcanvactionproject.Activity.Main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.a2021edcanvactionproject.R
@@ -10,13 +11,21 @@ import com.example.a2021edcanvactionproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    lateinit var navController : NavController
+
+    val a = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val bottomNav = binding.navBar
-        val navController = findNavController(R.id.fragmentContainerView)
+        navController = findNavController(R.id.fragmentContainerView)
 
         bottomNav.setupWithNavController(navController)
     }
+
+//    fun gotoMain(){
+//        navController.navigate(R.id.action_addTodoFragment_to_mainFragment)
+//    }
 }
--- app/src/main/java/com/example/a2021edcanvactionproject/Fragment/AddTodo/AddTodoFragment.kt
+++ app/src/main/java/com/example/a2021edcanvactionproject/Fragment/AddTodo/AddTodoFragment.kt
@@ -12,6 +12,8 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
+import com.example.a2021edcanvactionproject.Activity.Main.MainActivity
 import com.example.a2021edcanvactionproject.R
 import com.example.a2021edcanvactionproject.databinding.FragmentAddTodoBinding
 import com.example.a2021edcanvactionproject.Model.Time
@@ -21,6 +23,12 @@ import com.example.a2021edcanvactionproject.Model.addTodoData
 class AddTodoFragment : Fragment() {
     lateinit var binding : FragmentAddTodoBinding
     var choiceSetTime = true //시간, 세트 선택, true면 시간, false면 세트
+
+    lateinit var kind: String
+    lateinit var set: String
+    lateinit var setCount: String
+    lateinit var timeM: String
+    lateinit var timeS: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,6 +44,7 @@ class AddTodoFragment : Fragment() {
 
         return root
     }
+
 
     private fun ActivityInputEvent() = with(binding){
         btnAddTodoAddTodo.setOnClickListener {
@@ -76,44 +85,19 @@ class AddTodoFragment : Fragment() {
     }
 
     private fun saveTodoData() {
-        var kind: String
-        var set: String
-        var setCount: String
-        var timeM: String
-        var timeS: String
-
         with(binding) {
             kind = spinAddTodoChoice.selectedItem.toString();
             set = edtAddTodoSet.text.toString()// as Int
             setCount = edtAddTodoSetCount.text.toString()
             timeM = edtAddTodoTimeM.text.toString()
             timeS = edtAddTodoTimeS.text.toString()
-
-
-            if (kind == "운동 종류") {
-                Toast.makeText(requireContext(), "운동 종류를 선택해주세요.", Toast.LENGTH_LONG).show()
-                return
-            }
-            if (!choiceSetTime && set.isEmpty()) {
-                edtAddTodoSet.error = "세트를 입력하세요."
-                return
-            }
-            if (!choiceSetTime && setCount.isEmpty()) {
-                edtAddTodoSetCount.error = "한세트당 할 개수를 입력하세요."
-                return
-            }
-            if (choiceSetTime && timeM == "") {
-                edtAddTodoTimeM.error = "운동할 시간을 입력하세요."
-                return
-            }
-            if (choiceSetTime && timeS == "") {
-                edtAddTodoTimeS.error = "운동할 시간을 입력하세요."
-                return
-            }
         }
+
+        if(addTodoInputEmptyCheck()) {return}
+
         val todo = Todo(
             kind = kind,
-            st = choiceSetTime
+            st = choiceSetTime,
         )
 
         if (choiceSetTime) {
@@ -125,5 +109,30 @@ class AddTodoFragment : Fragment() {
 
         addTodoData(todoData = todo)
         Toast.makeText(requireContext(), "운동을 추가하였습니다.", Toast.LENGTH_LONG).show()
+
+        (activity as MainActivity).navController.navigate(R.id.action_addTodoFragment_to_mainFragment)
+    }
+
+    fun addTodoInputEmptyCheck() : Boolean = with(binding){
+        if (kind == "운동 종류") {
+            Toast.makeText(requireContext(), "운동 종류를 선택해주세요.", Toast.LENGTH_LONG).show()
+        }
+        else if (!choiceSetTime && set.isEmpty()) {
+            edtAddTodoSet.error = "세트를 입력하세요."
+        }
+        else if (!choiceSetTime && setCount.isEmpty()) {
+            edtAddTodoSetCount.error = "한세트당 할 개수를 입력하세요."
+        }
+        else if (choiceSetTime && timeM == "") {
+            edtAddTodoTimeM.error = "운동할 시간을 입력하세요."
+        }
+        else if (choiceSetTime && timeS == "") {
+            edtAddTodoTimeS.error = "운동할 시간을 입력하세요."
+        }
+        else{
+            return false
+        }
+
+        return true
     }
 }
